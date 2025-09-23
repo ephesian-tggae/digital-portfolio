934e4882cb979a0a9562ee879c5aed8d4ac2b049
ephesian-tggae <ephedio@icloud.com>
2025-09-23 17:25:51 -0500

    Added a floating bottom nav bar

--- src/script.js
+++ src/script.js
@@ -5,6 +5,25 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".floating-nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
 
 
 
